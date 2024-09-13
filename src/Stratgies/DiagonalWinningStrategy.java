784a49dce74cdc02602eb0b5e5e1c2cd1d42f48f
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2024-09-13 09:21:57 +0530

    winning stratagies added

---
+++ src/Stratgies/DiagonalWinningStrategy.java
@@ -0,0 +1,40 @@
+package Stratgies;
+
+import Models.Board;
+import Models.Move;
+import Models.Symbol;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DiagonalWinningStrategy implements WinningStrategy {
+    Map<Symbol, Integer> leftDiag = new HashMap<>();
+    Map<Symbol, Integer> rightDiag = new HashMap<>();
+
+    @Override
+    public boolean checkWinner(Move move, Board board) {
+        int col = move.getCell().getCol();
+        int row = move.getCell().getRow();
+        Symbol symbol = move.getPlayer().getSymbol();
+        boolean isWinner = false;
+
+        if(row == col) {
+            int countOfSymbol = leftDiag.getOrDefault(symbol, 0) + 1;
+            leftDiag.put(symbol, countOfSymbol);
+
+            if(countOfSymbol == board.getSize()) {
+                isWinner = true;
+            }
+        }
+
+        if(row + col == board.getSize() - 1) {
+            int countOfSymbol = rightDiag.getOrDefault(symbol, 0) + 1;
+            rightDiag.put(symbol, countOfSymbol);
+
+            if(countOfSymbol == board.getSize()) {
+                isWinner = true;
+            }
+        }
+        return isWinner;
+    }
+}
---
+++ src/Stratgies/RowWinningStrategy.java
@@ -0,0 +1,29 @@
+package Stratgies;
+
+import Models.Board;
+import Models.Move;
+import Models.Symbol;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RowWinningStrategy implements WinningStrategy {
+    private Map<Integer, Map<Symbol, Integer>> map = new HashMap<>();
+
+    @Override
+    public boolean checkWinner(Move move, Board board) {
+        int row = move.getCell().getRow();
+        Symbol symbol =  move.getPlayer().getSymbol();
+
+        Map<Symbol, Integer> countMap = map.getOrDefault(row, new HashMap<>());
+
+        int countOfSymbol = countMap.getOrDefault(symbol, 0) + 1;
+        countMap.put(symbol, countOfSymbol);
+
+        if (countOfSymbol == board.getSize()) {
+            return  true;
+        }
+
+        return false;
+    }
+}
